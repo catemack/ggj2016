5a2aca89d522fb87a724b061b4cd517971a9c7b4
Cate Mackenzie <chase.mackenzie@gmail.com>
2016-01-30 15:42:20 -0800

    Basic runner with 2 heights of obstacles

---
+++ GGJ 2016/Assets/Scripts/ObstacleScript.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ObstacleScript : MonoBehaviour
+{
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            Debug.Break();
+        }
+    }
+}
