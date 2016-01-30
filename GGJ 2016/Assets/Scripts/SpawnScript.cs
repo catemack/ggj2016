e82919fb9ac53021024c6347a06b7d004347a7bb
Cate Mackenzie <chase.mackenzie@gmail.com>
2016-01-30 14:37:00 -0800

    Added Assets

---
+++ GGJ 2016/Assets/Scripts/SpawnScript.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpawnScript : MonoBehaviour {
+
+    public GameObject obj;
+    public float spawnRate = 0.5f;
+    
+	void Start () {
+        Spawn();
+	}
+	
+	void Spawn()
+    {
+        Instantiate(obj, transform.position, Quaternion.identity);
+        Invoke("Spawn", spawnRate);
+    }
+}
